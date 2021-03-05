58be13cd3f508f758ff5a90013db9cf518167671
Calvin Chan <calvinchan87@gmail.com>
2021-03-05 02:33:34 +0000

    uploading implement letterPositions assignment with one known issue

---
+++ letterPositions.js
@@ -0,0 +1,32 @@
+const assertArraysEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x1F600) + ` Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(String.fromCodePoint(0x1F613) + ` Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  
+  for (x = 0; x < sentence.length; x++) {
+    results[sentence[x]] = [];
+  } 
+
+  for (x = 0; x < sentence.length; x++) {
+    results[sentence[x]].push(x);
+  } 
+
+  return results;
+};
+
+const string = "hello";
+
+const result = letterPositions(string, { a:true, b:true, c:true, d:true, e:true, f:true, g:true, h:true, i:true, j:true, k:true, l:true, m:true, n:true, o:true, p:true, q:true, r:true, s:true, t:true, u:true, v:true, w:true, x:true, y:true, z:true })
+
+console.log(result);
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+assertArraysEqual(letterPositions("hello").o, [4]);
