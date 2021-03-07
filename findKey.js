bebec98b6f7ebd87213be26a2b5b067bbea4a584
Calvin Chan <calvinchan87@gmail.com>
2021-03-07 15:38:23 +0000

    uploading findKey assignment - could be refactored

---
+++ findKey.js
@@ -0,0 +1,41 @@
+
+
+const findKey = function (list, ratingsfunc) {
+  let arrayvalues = Object.values(list);
+  let arraykeys = Object.keys(list);
+  for (i = 0; i < arrayvalues.length; i++) {
+    if (ratingsfunc(arrayvalues[i]) === true) {
+      return arraykeys[i];
+    }
+  }
+}
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x1F600) + ` Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(String.fromCodePoint(0x1F613) + ` Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual(
+  findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 2)
+  , "noma");
