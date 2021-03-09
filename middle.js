7b8d938049f68deb28eade55db91e7c26166fea9
Calvin Chan <calvinchan87@gmail.com>
2021-03-09 19:45:17 +0000

    modularizing and refactoring middle file

--- middle.js
+++ middle.js
@@ -1,3 +1,5 @@
+const assertArraysEqual = require('./assertArraysEqual');
+
 // FUNCTION IMPLEMENTATION
 const middle = function(anyArray) {
   let newArray = [];
@@ -11,24 +13,4 @@ const middle = function(anyArray) {
   } return newArray;
 }
 
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(String.fromCodePoint(0x1F600) + ` Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(String.fromCodePoint(0x1F613) + ` Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  if (JSON.stringify(array1) == JSON.stringify(array2)) {
-    return true;
-   } else return false;
-};
-
-// TEST CODE
-assertArraysEqual(middle([1]), []) // => []
-assertArraysEqual(middle([1, 2]), []) // => []
-assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
+module.exports = middle;
