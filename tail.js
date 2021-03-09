82510c86be86458991a98a846cddca2817494637
Calvin Chan <calvinchan87@gmail.com>
2021-03-09 18:10:00 +0000

    modularizing and refactoring tail file

--- tail.js
+++ tail.js
@@ -3,15 +3,4 @@ const tail = function(newarray) {
   console.log(newarray.slice(1));
 }
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(String.fromCodePoint(0x1F600) + ` Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(String.fromCodePoint(0x1F613) + ` Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
