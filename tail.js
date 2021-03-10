8b8543712a0fb64d38c8f13274fa7f3f7ff82552
Calvin Chan <calvinchan87@gmail.com>
2021-03-10 12:51:26 +0000

    Refactor with Mocha and Chai

--- tail.js
+++ tail.js
@@ -1,6 +1,7 @@
 // FUNCTION IMPLEMENTATION
 const tail = function(newarray) {
-  console.log(newarray.slice(1));
+  // console.log(newarray.slice(1));
+  return newarray.slice(1);
 }
 
 module.exports = tail;
